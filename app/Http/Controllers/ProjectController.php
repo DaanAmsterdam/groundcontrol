485c561b10706f7444535a42235bdbe4b5117e77
DaanAmsterdam <daan227@gmail.com>
2018-12-22 14:57:24 +0100

    Projects initial setup

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+
+    /**
+     * @param Project $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        Project::create($attributes);
+
+        return redirect('/projects');
+    }
+}
---
+++ app/Project.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $guarded = [];
+
+    /**
+     * @return string
+     */
+    public function path()
+    {
+        return "/projects/{$this->id}";
+    }
+}
