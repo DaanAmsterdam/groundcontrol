9d290f0a26e172012a22985852a772b0c7367449
DaanAmsterdam <daan227@gmail.com>
2019-01-18 23:01:38 +0100

    16: Notes and Policies

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.app')
 
 @section('content')
-    <header class="flex items-center mb-3 py-4">
+    <header class="flex items-center mb-3 pb-4">
         <div class="flex justify-between items-end w-full">
             <p class="text-grey text-sm font-normal">
                 <a href="/projects" class="text-grey text-sm font-normal no-underline hover:underline">My Projects</a>
@@ -46,7 +46,19 @@
                     <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
 
                     {{-- general notes --}}
-                    <textarea class="card w-full" style="min-height: 200px">Lorem ipsum.</textarea>
+                    <form method="POST" action="{{ $project->path() }}">
+                        @csrf
+                        @method('PATCH')
+
+                        <textarea
+                                name="notes"
+                                class="card w-full mb-4"
+                                style="min-height: 200px"
+                                placeholder="Anything special that you want to make a note of?"
+                        >{{ $project->notes }}</textarea>
+
+                        <button type="submit" class="button">Save</button>
+                    </form>
                 </div>
             </div>
 
@@ -55,5 +67,6 @@
             </div>
         </div>
     </main>
+
 
 @endsection
