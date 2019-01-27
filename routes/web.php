2babf7ed2d8fc14066748da4d864f208fa0d0803
DaanAmsterdam <daan227@gmail.com>
2019-01-27 23:58:01 +0100

    re-route the homepage

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
 
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'ProjectController@index');
     Route::get('/projects', 'ProjectController@index');
     Route::get('/projects/create', 'ProjectController@create');
     Route::get('/projects/{project}', 'ProjectController@show');
